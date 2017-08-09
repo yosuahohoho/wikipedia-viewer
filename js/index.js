25c83a1fd98f4a86180dc3294c6bdf232144beee
yosuahohoho <yosuahohoho@gmail.com>
2017-08-09 13:15:58 +0700

    add new style

--- js/index.js
+++ js/index.js
@@ -63,8 +63,8 @@ function displayToHtml(searchResults) {
 
 function addPager() {
     var htmlPager = "<div class='pager_section'><ul class='pager'> \
-                <li><a href='#'><i class='fa fa-angle-left' aria-hidden='true'></i></a></li> \
-                <li><a href='#'><i class='fa fa-angle-right' aria-hidden='true'></i></a></li> \
+                <li><a role='button'><i class='fa fa-angle-left' aria-hidden='true'></i></a></li> \
+                <li><a id='buttonNext' role='button'><i class='fa fa-angle-right' aria-hidden='true'></i></a></li> \
                 </ul></div>";
 
     return htmlPager;
@@ -72,29 +72,32 @@ function addPager() {
 
 $(document).ready(function() {
 
+  $("#myModal").modal("show");
+
   var searchParameter = {
         action: 'query',
         format: 'json',
         prop:'info',
         origin: '*',
         list: 'search',
-        continue: '',
+        continue: '-||',
         srsearch:'',
-        srlimit: '6',
+        srlimit:'6',
+        sroffset:'12',
         srwhat: 'text',
         srprop: 'snippet',
 }
 
   get(searchParameter);
 
-  // if users click or press search button
-  $("#buttonGet").on("click", get(searchParameter));
-
   // if users press enter
   $("#searchBox").on("keypress", function(e) {
       if (e.which == 13) {
+          e.preventDefault();
           get(searchParameter);
       }
     });
+
+  //$("#buttonNext").on("click", nextEntries(searchParameter));
 
 })
